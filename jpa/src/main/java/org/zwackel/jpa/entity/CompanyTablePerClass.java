2fdda2fcd3a97d161b56e204919f339ec030a8a4
christian <christian@ThinkPad-Christian>
2015-02-10 21:06:40 +0100

    add toString and format

--- jpa/src/main/java/org/zwackel/jpa/entity/CompanyTablePerClass.java
+++ jpa/src/main/java/org/zwackel/jpa/entity/CompanyTablePerClass.java
@@ -5,13 +5,18 @@ import javax.persistence.*;
 @Entity
 @Access(AccessType.FIELD)
 public class CompanyTablePerClass extends PersonTablePerClass {
-private String name;
+    private String name;
 
-public String getName() {
-    return name;
-}
+    public String getName() {
+        return name;
+    }
 
-public void setName(String name) {
-    this.name = name;
-}
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%s[id=%d; name=%s]", CompanyTablePerClass.class.getCanonicalName(), getId(), getName());
+    }
 }
