1b26840d100c802f5471845d6067768f9da44b89
christian <christian@ThinkPad-Christian>
2015-03-10 20:31:18 +0100

    add welcome message

--- cdi/src/main/java/org/zwackel/cdi/service/BarInfo.java
+++ cdi/src/main/java/org/zwackel/cdi/service/BarInfo.java
@@ -6,7 +6,12 @@ public class BarInfo implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private static int count;
     public String getName(){
         return "Endstation";
+    }
+    
+    public String getWelcomeMessage(){
+        return String.format("Herzlich willkommen! Sie sind der %d. Besucher in der Endstation", ++count);
     }
 }
