a258e4008f6d01e87ec3ce39da6c985d889dfdef
trex667 <froehlich.ch@gmail.com>
2015-05-25 19:19:21 +0200

    add changes of chapter one

--- vaadin-examples/src/main/java/de/schreibvehler/MyVaadinUI.java
+++ vaadin-examples/src/main/java/de/schreibvehler/MyVaadinUI.java
@@ -1,32 +1,45 @@
 package de.schreibvehler;
 
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.Label;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
+import com.vaadin.shared.ui.label.ContentMode;
+import com.vaadin.ui.*;
 
 /**
  * The Application's "main" class
  */
 @SuppressWarnings("serial")
-public class MyVaadinUI extends UI
-{
+public class MyVaadinUI extends UI {
 
     @Override
     protected void init(VaadinRequest request) {
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
         setContent(layout);
-        
+
+        TextField nameField1 = new TextField("name one");
+        TextField nameField2 = new TextField("name two");
+
+        layout.addComponent(nameField1);
+        layout.addComponent(nameField2);
+
         Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
+        button.addClickListener((event) -> {
+            String message = getFunnyPhrase(nameField1.getValue(), nameField2.getValue());
+            Label label = new Label(message);
+            label.setContentMode(ContentMode.HTML);
+            layout.addComponent(label);
+            Notification.show("Button click successfully finished!");
         });
         layout.addComponent(button);
+    }
+
+    private String getFunnyPhrase(String name1, String name2) {
+        String[] verbs = new String[] { "eats", "melts", "breaks", "washes", "sells" };
+        String[] bodyPart = new String[] { "head", "hands", "waist", "eyes", "elboes" };
+
+        return String.format("%s %s %s's %s", name1, verbs[(int) Math.random() * 100 % verbs.length], name2,
+                bodyPart[(int) Math.random() * 100 % bodyPart.length]);
+
     }
 
 }
