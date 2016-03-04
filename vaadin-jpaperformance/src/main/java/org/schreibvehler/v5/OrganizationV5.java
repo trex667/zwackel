b2b563fa0201370306e371efdf32c071688ae9ad
Christian Froehlich <c.froehlich@seeburger.de>
2016-03-04 11:13:53 +0100

    change creation of organization in V5

--- vaadin-jpaperformance/src/main/java/org/schreibvehler/v5/OrganizationV5.java
+++ vaadin-jpaperformance/src/main/java/org/schreibvehler/v5/OrganizationV5.java
@@ -1,14 +1,17 @@
 package org.schreibvehler.v5;
 
+
 import java.util.*;
 
 import javax.persistence.*;
 
 import org.schreibvehler.boundary.*;
 
+
 @Entity
 @Access(AccessType.FIELD)
-public class OrganizationV5 implements Organization {
+public class OrganizationV5 implements Organization
+{
 
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
@@ -18,30 +21,55 @@ public class OrganizationV5 implements Organization {
     @ManyToMany
     private Set<UserV5> users;
 
+
     @Override
-    public String getName() {
+    public String getName()
+    {
         return name;
     }
 
+
     @Override
-    public Set<User> getUsers() {
+    public Set<User> getUsers()
+    {
         return Collections.unmodifiableSet(users);
     }
 
-    public Integer getId() {
+
+    public Integer getId()
+    {
         return id;
     }
 
-    public void setId(Integer id) {
+
+    public void setId(Integer id)
+    {
         this.id = id;
     }
 
-    public void setName(String name) {
+
+    public void setName(String name)
+    {
         this.name = name;
     }
 
-    public void setUsers(Set<UserV5> users) {
+
+    public void setUsers(Set<UserV5> users)
+    {
         this.users = users;
+    }
+
+
+    public void addUsers(Set<UserV5> users)
+    {
+        if (this.users == null)
+        {
+            this.users = users;
+        }
+        else
+        {
+            this.users.addAll(users);
+        }
     }
 
 }
--- vaadin-jpaperformance/src/main/java/org/schreibvehler/v5/UserServiceV5.java
+++ vaadin-jpaperformance/src/main/java/org/schreibvehler/v5/UserServiceV5.java
@@ -1,27 +1,45 @@
 package org.schreibvehler.v5;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
-import javax.persistence.*;
+import javax.persistence.EntityGraph;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.time.DateUtils;
-import org.schreibvehler.boundary.*;
+import org.schreibvehler.boundary.Address;
+import org.schreibvehler.boundary.DataUtils;
+import org.schreibvehler.boundary.Organization;
+import org.schreibvehler.boundary.Result;
+import org.schreibvehler.boundary.TimeInterval;
+import org.schreibvehler.boundary.User;
+import org.schreibvehler.boundary.UserService;
+
 
 @Stateless
-public class UserServiceV5 implements UserService {
+public class UserServiceV5 implements UserService
+{
 
     private static final Logger LOG = Logger.getLogger(UserServiceV5.class.getName());
 
     @PersistenceContext
     private EntityManager em;
 
+
     @Override
-    public Result<User> findAllUsers() {
+    public Result<User> findAllUsers()
+    {
         long start = System.currentTimeMillis();
-        
+
         EntityGraph<UserV5> graph = em.createEntityGraph(UserV5.class);
         graph.addAttributeNodes("addresses");
         graph.addAttributeNodes("organizations");
@@ -33,41 +51,65 @@ public class UserServiceV5 implements UserService {
         return new Result<User>(new TimeInterval(start, end), list);
     }
 
+
     @Override
-    public Result<User> createTestData(int count) {
+    public Result<User> createTestData(int count)
+    {
         long start = System.currentTimeMillis();
         List<User> resultList = new ArrayList<>();
         Set<UserV5> entities = new HashSet<>();
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < count; i++)
+        {
             UserV5 user = new UserV5();
             user.setName(RandomStringUtils.randomAlphabetic(20));
-            user.setBirthdate(
-                    DateUtils.addYears(new Date(), Integer.parseInt(RandomStringUtils.random(2, "123456789")) * -1));
+            user.setBirthdate(DateUtils.addYears(new Date(), Integer.parseInt(RandomStringUtils.random(2, "123456789")) * -1));
             em.persist(user);
 
             createAddresses(user);
             resultList.add(user);
             entities.add(user);
         }
-        createOrganizations(entities);
+        addToOrganizations(entities);
         long end = System.currentTimeMillis();
         return new Result<User>(new TimeInterval(start, end), resultList);
     }
 
-    private void createOrganizations(Set<UserV5> users) {
-        for (int i = 0; i < 100; i++) {
-            OrganizationV5 org = new OrganizationV5();
-            org.setUsers(users);
-            org.setName(RandomStringUtils.randomAlphabetic(20));
-            em.persist(org);
+
+    private void addToOrganizations(Set<UserV5> users)
+    {
+        String select = "SELECT o FROM OrganizationV5 o";
+        TypedQuery<OrganizationV5> organizations = em.createQuery(select, OrganizationV5.class);
+        if (organizations.getResultList() == null || organizations.getResultList().size() == 0)
+        {
+            createOrganizations(100);
+        }
+        organizations = em.createQuery(select, OrganizationV5.class);
+        for (OrganizationV5 org : organizations.getResultList())
+        {
+            org.addUsers(users);
+            em.merge(org);
         }
 
     }
 
-    private List<AddressV5> createAddresses(UserV5 user) {
+
+    private void createOrganizations(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            OrganizationV5 org = new OrganizationV5();
+            org.setName(RandomStringUtils.randomAlphabetic(20));
+            em.persist(org);
+        }
+    }
+
+
+    private List<AddressV5> createAddresses(UserV5 user)
+    {
         List<AddressV5> result = new ArrayList<>();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             AddressV5 address = new AddressV5();
             address.setUser(user);
             address.setCity(DataUtils.getRandomCity());
@@ -80,8 +122,10 @@ public class UserServiceV5 implements UserService {
         return result;
     }
 
+
     @Override
-    public Result<Address> findAllAddresses(Integer userId) {
+    public Result<Address> findAllAddresses(Integer userId)
+    {
         long start = System.currentTimeMillis();
         TypedQuery<Address> query = em.createQuery("SELECT a FROM AddressV5 a WHERE a.user.id=:userid", Address.class);
         query.setParameter("userid", userId);
@@ -91,11 +135,12 @@ public class UserServiceV5 implements UserService {
         return new Result<Address>(new TimeInterval(start, end), resultList);
     }
 
+
     @Override
-    public Result<Organization> findAllOrganizations(Integer userId) {
+    public Result<Organization> findAllOrganizations(Integer userId)
+    {
         long start = System.currentTimeMillis();
-        TypedQuery<Organization> query = em.createQuery("SELECT o FROM OrganizationV5 o WHERE :user MEMBER OF o.users",
-                Organization.class);
+        TypedQuery<Organization> query = em.createQuery("SELECT o FROM OrganizationV5 o WHERE :user MEMBER OF o.users", Organization.class);
         query.setParameter("user", em.find(UserV5.class, userId));
 
         List<Organization> resultList = query.getResultList();
