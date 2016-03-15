e8022098629f57869cd83e8489fb85c3cb537fef
trex667 <froehlich.ch@gmail.com>
2016-03-15 20:34:41 +0100

    add junit dependency

--- vaadin-jpaperformance/src/main/java/org/schreibvehler/boundary/TimeInterval.java
+++ vaadin-jpaperformance/src/main/java/org/schreibvehler/boundary/TimeInterval.java
@@ -1,28 +1,23 @@
 package org.schreibvehler.boundary;
 
 
-public class TimeInterval
-{
+public class TimeInterval {
 
     private final long start;
     private final long end;
 
 
-    public TimeInterval(long start, long end)
-    {
+    public TimeInterval(long start, long end) {
         this.start = start;
         this.end = end;
     }
 
 
-    public long getEnd()
-    {
+    public long getEnd() {
         return end;
     }
 
-
-    public long getStart()
-    {
+    public long getStart() {
         return start;
     }
 
